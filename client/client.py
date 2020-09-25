795bfc65a7469bb57eb400931bb2430cdb1995e2
Ali Benkassou <a.benkassou1@gmail.com>
2020-09-26 00:14:42 +0200

    Add args

--- client/client.py
+++ client/client.py
@@ -1,29 +1,57 @@
 """"""
+
+
+import argparse
 import subprocess
 from time import sleep 
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--linux",
+    action="store_true"
+)
+parser.add_argument(
+    "--mac_os",
+    action="store_true"
+)
+parser.add_argument(
+    "--interval",
+    default=10,
+    help="Data scrapping frequency (in seconds)"
+)
+
+
 def client():
-    command = "scrap/MACOS/scrap.sh"
+    if args.mac_os:
+        command = "scrap/macOS/scrap.sh"
+    elif args.linux:
+        command = "scrap/Linux/scrap.sh"
+    else:
+        raise AssertionError("OS not specified") 
     result = subprocess.run(
         command,
         shell=True,
         stdout=subprocess.PIPE
     )
-    with open("/tmp/passage2.txt", "a") as f:
-        f.write("Un passage ici\n")
+   
+    # Format output 
     if result.returncode == 0:
         data = result.stdout.decode("utf-8")
-        # split rows
         columns = data.split("\n")
-        print(columns)
         for col in columns[:-1]:
             print(col.split(","))
+
+    # Send payload
+    # requests
+
 
 def main():
     while True:
         client()
-        sleep(10)
+        sleep(int(args.interval))
 
 
 if __name__ == "__main__":
+    args = parser.parse_args()
     main()
